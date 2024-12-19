bca4a15484f1e84fbcc99ee1f31d3768983160a7
NabilF01 <nabilfarhat01@gmail.com>
2024-12-19 13:37:07 +0100

    feat: end liste de courses

--- js/courses.js
+++ js/courses.js
@@ -1,2 +1,31 @@
-let valueInput = document.getElementById('courseElement');
 
+let input = document.getElementById('courseElement');
+let button = document.getElementById('btn');
+
+
+let main = document.querySelector('main');
+
+
+button.addEventListener('click', () => {
+    let inputValue = input.value.trim();
+
+    if (inputValue === "") {
+        alert("Veuillez entrer un élément !");
+        return;
+    }
+
+    let ul = document.querySelector('ul');
+    if (!ul) {
+        ul = document.createElement('ul');
+        main.appendChild(ul);
+    }
+
+
+    let li = document.createElement('li');
+    li.textContent = inputValue;
+
+
+    ul.appendChild(li);
+
+    input.value = "";
+});
