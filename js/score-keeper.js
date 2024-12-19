a81f536b00345d9e0133b1705507aaca2b68442f
NabilF01 <nabilfarhat01@gmail.com>
2024-12-19 15:45:06 +0100

    feat: adding score keeper

---
+++ js/score-keeper.js
@@ -0,0 +1,16 @@
+let btnP1 = document.getElementById('p1');
+let btnP2 = document.getElementById('p2');
+let reset = document.getElementById('reset');
+let input = document.getElementById('playing');
+
+btnP1.addEventListener('click', () => {
+
+});
+
+btnP2.addEventListener('click', () => {
+
+});
+
+reset.addEventListener('click', () => {
+
+});
